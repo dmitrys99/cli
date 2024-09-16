a33c21f9bcfca07fb85408d7d77e0762cbc07696
Dmitry Solomennikov <dmitrys99@mail.ru>
2024-09-16 22:41:19 +0300

    Russian texts
    Signed-off-by: d.solomennikov <d.solomennikov@d-solomennikov.local>

--- builtin.go
+++ builtin.go
@@ -6,7 +6,7 @@ import (
 
 // Helper is builtin Help flag
 type Helper struct {
-	Help bool `cli:"!h,help" usage:"display help information" json:"-"`
+	Help bool `cli:"!h,help" usage:"показать справочную информацию" json:"-"`
 }
 
 // AutoHelp implements AutoHelper interface
@@ -16,7 +16,7 @@ func (h Helper) AutoHelp() bool {
 
 // Helper2 is builtin Help flag
 type Helper2 struct {
-	Help bool `cli:"!h,help" usage:"Display help information" json:"-"`
+	Help bool `cli:"!h,help" usage:"Показать справочную информацию" json:"-"`
 }
 
 // AutoHelp implements AutoHelper interface
@@ -26,14 +26,14 @@ func (h Helper2) AutoHelp() bool {
 
 // Deprecated: Addr is builtin host,port flag
 type Addr struct {
-	Host string `cli:"host" usage:"specify host" dft:"0.0.0.0"`
-	Port uint16 `cli:"port" usage:"specify port" dft:"8080"`
+	Host string `cli:"host" usage:"укажите host" dft:"0.0.0.0"`
+	Port uint16 `cli:"port" usage:"укажите port" dft:"8080"`
 }
 
 // Deprecated: AddrWithShort is builtin host,port flag contains short flag
 type AddrWithShort struct {
-	Host string `cli:"H,host" usage:"specify host" dft:"0.0.0.0"`
-	Port uint16 `cli:"p,port" usage:"specify port" dft:"8080"`
+	Host string `cli:"H,host" usage:"укажите host" dft:"0.0.0.0"`
+	Port uint16 `cli:"p,port" usage:"укажите port" dft:"8080"`
 }
 
 // Deprecated: ToString ...
